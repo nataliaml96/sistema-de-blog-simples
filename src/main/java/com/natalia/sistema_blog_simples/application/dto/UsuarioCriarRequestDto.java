63d1c31c69b6b961db7716c47c88db226a4bde95
natalia <doublenat96@gmail.com>
2025-11-05 23:37:14 -0300

    refatora projeto

--- src/main/java/com/natalia/sistema_blog_simples/application/dto/UsuarioCriarRequestDto.java
+++
@@ -1,4 +0,0 @@
-package com.natalia.sistema_blog_simples.application.dto;
-
-public record UsuarioCriarRequestDto(String nome, String email, String senha, String cpf) {
-}
--- src/main/java/com/natalia/sistema_blog_simples/domain/valueobjects/EnumStatusUsuario.java
+++
@@ -1,6 +0,0 @@
-package com.natalia.sistema_blog_simples.domain.valueobjects;
-
-public enum EnumStatusUsuario {
-
-    ATIVO, BLOQUEADO, EXCLUIDO
-}
